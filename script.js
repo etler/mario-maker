3d72d98b52aa4e4a4212a5934b0aebf0a8259340
Tim Etler <tmetler@gmail.com>
2019-07-12 11:07:54 -0700

    Initial version of mario maker 12 item challenge

---
+++ script.js
@@ -0,0 +1,259 @@
+nameMap = {
+  "games": {
+    "smb": "Super Mario Bros.",
+    "smb3": "Super Mario Bros. 3",
+    "smw": "Super Mario World",
+    "smbu": "New Super Mario Bros. U",
+    "sm3d": "Super Mario 3D World"
+  },
+  "types": {
+    "terrain": "Terrain",
+    "items": "Items",
+    "enemies": "Enemies",
+    "gizmos": "Gizmos"
+  },
+  "terrain": {
+    "block": "Block",
+    "bridge": "Bridge",
+    "clear_pipe": "Clear Pipe",
+    "cloud_block": "Cloud Block",
+    "donut_block": "Donut Block",
+    "gentle_slope": "Gentle Slope",
+    "ground": "Ground",
+    "hard_block": "Hard Block",
+    "hidden_block": "Hidden Block",
+    "ice_block": "Ice Block",
+    "mushroom_platform": "Mushroom Platform",
+    "note_block": "Note Block",
+    "pipe": "Pipe",
+    "question_block": "? Block",
+    "semisolid_platform": "Semisolid Platform",
+    "spike_block": "Spike Block",
+    "spike_trap": "Spike Trap",
+    "steep_slope": "Steep Slope"
+  },
+  "items": {
+    "big_mushroom": "Big Mushroom",
+    "cape_feather": "Cape Feather",
+    "coin": "Coin",
+    "fifty_coin": "50-Coin",
+    "fire_flower": "Fire Flower",
+    "one_up_mushroom": "1-Up Mushroom",
+    "pink_coin": "Pink Coin",
+    "propeller_mushroom": "Propeller Mushroom",
+    "rotten_mushroom": "Rotten Mushroom",
+    "shoe_goomba": "Shoe Goomba",
+    "stiletto_goomba": "Stiletto Goomba",
+    "super_bell": "Super Bell",
+    "super_hammer": "Super Hammer",
+    "super_leaf": "Super Leaf",
+    "super_mushroom": "Super Mushroom",
+    "super_star": "Super Star",
+    "superball_flower": "Superball Flower",
+    "ten_coin": "10-Coin",
+    "thirty_coin": "30-Coin",
+    "yoshis_egg": "Yoshi's Egg"
+  },
+  "enemies": {
+    "angry_sun": "Angry Sun",
+    "angry_wiggler": "Angry Wiggler",
+    "ant_trooper": "Ant Trooper",
+    "blooper": "Blooper",
+    "blooper_nanny": "Blooper Nanny",
+    "bob_omb": "Bob-omb",
+    "boo": "Boo",
+    "boo_buddies": "Boo Buddies",
+    "boom_boom": "Boom Boom",
+    "bowser": "Bowser",
+    "bowser_jr": "Bowser Jr.",
+    "bully": "Bully",
+    "buzzy_beetle": "Buzzy Beetle",
+    "buzzy_beetle_shell": "Buzzy Beetle Shell",
+    "chain_chomp": "Chain Chomp",
+    "charvaargh": "Charvaargh",
+    "cheep_cheep": "Cheep Cheep",
+    "dry_bones": "Dry Bones",
+    "dry_bones_shell": "Dry Bones Shell",
+    "fire_bro": "Fire Bro",
+    "fire_koopa_clown_car": "Fire Koopa Clown Car",
+    "fire_piranha_plant": "Fire Piranha Plant",
+    "fish_bone": "Fish Bone",
+    "galoomba": "Galoomba",
+    "goomba": "Goomba",
+    "goombrat": "Goombrat",
+    "goombud": "Goombud",
+    "hammer_bro": "Hammer Bro",
+    "hop_chops": "Hop-Chops",
+    "horned_ant_trooper": "Horned Ant Trooper",
+    "jumping_piranha_plant": "Jumping Piranha Plant",
+    "koopa_clown_car": "Koopa Clown Car",
+    "koopa_troopa": "Koopa Troopa",
+    "koopa_troopa_car": "Koopa Troopa Car",
+    "lakitu": "Lakitu",
+    "lakitus_cloud": "Lakitu's Cloud",
+    "lava_bubble": "Lava Bubble",
+    "lit_bob_omb": "Lit Bob-omb",
+    "magikoopa": "Magikoopa",
+    "meowser": "Meowser",
+    "monty_mole": "Monty Mole",
+    "moon": "Moon",
+    "muncher": "Muncher",
+    "peepa": "Peepa",
+    "piranha_creeper": "Piranha Creeper",
+    "piranha_plant": "Piranha Plant",
+    "pom_pom": "Pom Pom",
+    "porcupuffer": "Porcupuffer",
+    "rocky_wrench": "Rocky Wrench",
+    "skipsqueak": "Skipsqueak",
+    "spike_top": "Spike Top",
+    "spiny": "Spiny",
+    "spiny_shell": "Spiny Shell",
+    "spiny_skipsqueak": "Spiny Skipsqueak",
+    "stingby": "Stingby",
+    "thwomp": "Thwomp",
+    "unchained_chomp": "Unchained Chomp",
+    "wiggler": "Wiggler"
+  },
+  "gizmos": {
+    "arrow_sign": "Arrow Sign",
+    "banzai_bill": "Banzai Bill",
+    "bill_blaster": "Bill Blaster",
+    "blinking_block": "Blinking Block",
+    "bulls_eye_banzai": "Bull's-Eye Banzai",
+    "bulls_eye_blaster": "Bull's-Eye Blaster",
+    "bumper": "Bumper",
+    "burner": "Burner",
+    "cannon": "Cannon",
+    "checkpoint_flag": "Checkpoint Flag",
+    "cloud_lift": "Cloud Lift",
+    "conveyor_belt": "Conveyor Belt",
+    "crate": "Crate",
+    "dotted_line_block": "Dotted-Line Block",
+    "exclamation_block": "! Block",
+    "fast_conveyor_belt": "Fast Conveyor Belt",
+    "fast_lava_lift": "Fast Lava Lift",
+    "fast_snake_block": "Fast Snake Block",
+    "fire_bar": "Fire Bar",
+    "flimsy_lift": "Flimsy Lift",
+    "grinder": "Grinder",
+    "icicle": "Icicle",
+    "key": "Key",
+    "key_door": "Key Door",
+    "lava_lift": "Lava Lift",
+    "lift": "Lift",
+    "mushroom_trampoline": "Mushroom Trampoline",
+    "on_off_switch": "ON/OFF Switch",
+    "one_way_wall": "One-Way Wall",
+    "p_switch": "P Switch",
+    "p_warp_door": "P Warp Door",
+    "pow_block": "POW Block",
+    "red_cannon": "Red Cannon",
+    "red_pow_block": "Red POW Block",
+    "seesaw": "Seesaw",
+    "skewer": "Skewer",
+    "snake_block": "Snake Block",
+    "swinging_claw": "Swinging Claw",
+    "track": "Track",
+    "track_block": "Track Block",
+    "trampoline": "Trampoline",
+    "tree": "Tree",
+    "twister": "Twister",
+    "vine": "Vine",
+    "warp_box": "Warp Box",
+    "warp_box_with_key": "Warp Box With Key",
+    "warp_door": "Warp Door"
+  }
+}
+
+gameObjects = {
+  "smb": {
+    "terrain": ["ground", "steep_slope", "gentle_slope", "pipe", "spike_trap", "mushroom_platform", "semisolid_platform", "bridge", "block", "question_block", "hard_block", "hidden_block", "donut_block", "note_block", "cloud_block", "ice_block"],
+    "items": ["coin", "ten_coin", "pink_coin", "super_mushroom", "fire_flower", "big_mushroom", "super_star", "one_up_mushroom", "shoe_goomba"],
+    "enemies": ["goomba", "koopa_troopa", "buzzy_beetle", "spike_top", "spiny", "blooper", "cheep_cheep", "piranha_plant", "muncher", "thwomp", "monty_mole", "rocky_wrench", "hammer_bro", "chain_chomp", "wiggler", "boo", "lava_bubble", "bob_omb", "dry_bones", "fish_bone", "magikoopa", "bowser", "bowser_jr", "boom_boom", "angry_sun", "lakitu", "koopa_clown_car"],
+    "gizmos": ["burner", "bill_blaster", "banzai_bill", "cannon", "icicle", "twister", "key", "warp_door", "p_switch", "pow_block", "trampoline", "vine", "arrow_sign", "checkpoint_flag", "lift", "lava_lift", "seesaw", "grinder", "bumper", "skewer", "swinging_claw", "on_off_switch", "dotted_line_block", "snake_block", "fire_bar", "one_way_wall", "conveyor_belt", "track"]
+  },
+  "smb3": {
+    "terrain": ["ground", "steep_slope", "gentle_slope", "pipe", "spike_trap", "mushroom_platform", "semisolid_platform", "bridge", "block", "question_block", "hard_block", "hidden_block", "donut_block", "note_block", "cloud_block", "ice_block"],
+    "items": ["coin", "ten_coin", "pink_coin", "super_mushroom", "fire_flower", "super_leaf", "super_star", "one_up_mushroom", "shoe_goomba"],
+    "enemies": ["goomba", "koopa_troopa", "buzzy_beetle", "spike_top", "spiny", "blooper", "cheep_cheep", "piranha_plant", "muncher", "thwomp", "monty_mole", "rocky_wrench", "hammer_bro", "chain_chomp", "wiggler", "boo", "lava_bubble", "bob_omb", "dry_bones", "fish_bone", "magikoopa", "bowser", "bowser_jr", "boom_boom", "angry_sun", "lakitu", "koopa_clown_car"],
+    "gizmos": ["burner", "bill_blaster", "banzai_bill", "cannon", "icicle", "twister", "key", "warp_door", "p_switch", "pow_block", "trampoline", "vine", "arrow_sign", "checkpoint_flag", "lift", "lava_lift", "seesaw", "grinder", "bumper", "skewer", "swinging_claw", "on_off_switch", "dotted_line_block", "snake_block", "fire_bar", "one_way_wall", "conveyor_belt", "track"]
+  },
+  "smw": {
+    "terrain": ["ground", "steep_slope", "gentle_slope", "pipe", "spike_trap", "mushroom_platform", "semisolid_platform", "bridge", "block", "question_block", "hard_block", "hidden_block", "donut_block", "note_block", "cloud_block", "ice_block"],
+    "items": ["coin", "ten_coin", "pink_coin", "super_mushroom", "fire_flower", "cape_feather", "super_star", "one_up_mushroom", "yoshis_egg"],
+    "enemies": ["galoomba", "koopa_troopa", "buzzy_beetle", "spike_top", "spiny", "blooper", "cheep_cheep", "jumping_piranha_plant", "muncher", "thwomp", "monty_mole", "rocky_wrench", "hammer_bro", "chain_chomp", "wiggler", "boo", "lava_bubble", "bob_omb", "dry_bones", "fish_bone", "magikoopa", "bowser", "bowser_jr", "boom_boom", "angry_sun", "lakitu", "koopa_clown_car"],
+    "gizmos": ["burner", "bill_blaster", "banzai_bill", "cannon", "icicle", "twister", "key", "warp_door", "p_switch", "pow_block", "trampoline", "vine", "arrow_sign", "checkpoint_flag", "lift", "lava_lift", "seesaw", "grinder", "bumper", "skewer", "swinging_claw", "on_off_switch", "dotted_line_block", "snake_block", "fire_bar", "one_way_wall", "conveyor_belt", "track"]
+  },
+  "smbu": {
+    "terrain": ["ground", "steep_slope", "gentle_slope", "pipe", "spike_trap", "mushroom_platform", "semisolid_platform", "bridge", "block", "question_block", "hard_block", "hidden_block", "donut_block", "note_block", "cloud_block", "ice_block"],
+    "items": ["coin", "ten_coin", "pink_coin", "super_mushroom", "fire_flower", "propeller_mushroom", "super_star", "one_up_mushroom", "yoshis_egg"],
+    "enemies": ["goomba", "koopa_troopa", "buzzy_beetle", "spike_top", "spiny", "blooper", "cheep_cheep", "piranha_plant", "muncher", "thwomp", "monty_mole", "rocky_wrench", "hammer_bro", "chain_chomp", "wiggler", "boo", "lava_bubble", "bob_omb", "dry_bones", "fish_bone", "magikoopa", "bowser", "bowser_jr", "boom_boom", "angry_sun", "lakitu", "koopa_clown_car"],
+    "gizmos": ["burner", "bill_blaster", "banzai_bill", "cannon", "icicle", "twister", "key", "warp_door", "p_switch", "pow_block", "trampoline", "vine", "arrow_sign", "checkpoint_flag", "lift", "lava_lift", "seesaw", "grinder", "bumper", "skewer", "swinging_claw", "on_off_switch", "dotted_line_block", "snake_block", "fire_bar", "one_way_wall", "conveyor_belt", "track"]
+  },
+  "sm3d": {
+    "terrain": ["ground", "steep_slope", "gentle_slope", "pipe", "clear_pipe", "spike_block", "semisolid_platform", "block", "question_block", "hard_block", "hidden_block", "donut_block", "cloud_block", "ice_block"],
+    "items": ["coin", "ten_coin", "pink_coin", "super_mushroom", "super_bell", "super_hammer", "fire_flower", "super_star", "one_up_mushroom"],
+    "enemies": ["goomba", "koopa_troopa", "ant_trooper", "spiny", "blooper", "cheep_cheep", "skipsqueak", "stingby", "piranha_plant", "piranha_creeper", "thwomp", "hammer_bro", "hop_chops", "boo", "lava_bubble", "bob_omb", "dry_bones", "fish_bone", "magikoopa", "meowser", "boom_boom", "charvaargh", "bully", "porcupuffer", "koopa_troopa_car"],
+    "gizmos": ["bill_blaster", "banzai_bill", "icicle", "twister", "on_off_switch", "conveyor_belt", "crate", "key", "warp_door", "warp_box", "p_switch", "pow_block", "trampoline", "arrow_sign", "checkpoint_flag", "cloud_lift", "exclamation_block", "snake_block", "blinking_block", "track_block", "tree", "mushroom_trampoline"]
+  }
+}
+
+let imagesHtml = ''
+
+for (let gameKey in gameObjects) {
+  imagesHtml += `<h1>${nameMap.games[gameKey]}</h1>`
+  for (let typeKey in gameObjects[gameKey]) {
+    imagesHtml += `<h2>${nameMap.types[typeKey]}</h2><div style="display: flex">`
+    for (let objectIndex = 0; objectIndex < gameObjects[gameKey][typeKey].length; objectIndex++) {
+      let objectList = gameObjects[gameKey][typeKey]
+      let objectKey = objectList[objectIndex]
+      let objectName = nameMap[typeKey][objectKey]
+      imagesHtml += `<div style="margin: 10px"><img src="./assets/${gameKey}/${typeKey}/${objectKey}.png"></img><h3>${objectName}</h3></div>`
+    }
+    imagesHtml += `</div>`
+  }
+}
+
+const getObject = function (gameKey, typeKey, objectKey) {
+  return {
+    name: nameMap[typeKey][objectKey],
+    image: `./assets/${gameKey}/${typeKey}/${objectKey}.png`
+  }
+}
+
+const getRandomObjects = function (count) {
+  let gameKeys = Object.keys(nameMap.games)
+  let gameKey = gameKeys[Math.floor(Math.random()*gameKeys.length)]
+  let objectList = []
+  for (let index = 0; index < count; index++) {
+    let lastLength = objectList.length
+    while(objectList.length === lastLength) {
+      let typeKeys = Object.keys(nameMap.types)
+      let typeKey = typeKeys[Math.floor(Math.random()*typeKeys.length)]
+      let typeObjectList = gameObjects[gameKey][typeKey]
+      let objectKey = typeObjectList[Math.floor(Math.random()*typeObjectList.length)]
+      let object = getObject(gameKey, typeKey, objectKey)
+      if (!objectList.find((fineObject) => fineObject.image === object.image)) {
+        objectList.push(object)
+      }
+    }
+  }
+  return objectList
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  let boxes = Array.prototype.slice.apply(document.body.querySelectorAll('.Box_hidden'))
+  for (let index = 0; index < boxes.length; index++) {
+    let box = boxes[index]
+    setTimeout(() => {
+      box.classList.remove('Box_hidden')
+    }, 25*index)
+  }
+  let boxImageList = Array.prototype.slice.apply(document.body.querySelectorAll('.Box-Image'))
+  let objects = getRandomObjects(boxImageList.length)
+  for (let index = 0; index < boxes.length; index++) {
+    let boxImage = boxImageList[index]
+    boxImage.src = objects[index].image
+  }
+  // document.body.innerHTML = imagesHtml
+})
